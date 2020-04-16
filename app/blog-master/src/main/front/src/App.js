b89401d196bd2ddd4ac6c38d0c3298c6eca1e8f4
ValtteriPeltonen <ValtteriPeltonen@users.noreply.github.com>
2020-04-16 21:24:22 +0300

    General update

--- app/blog-master/src/main/front/src/App.js
+++ app/blog-master/src/main/front/src/App.js
@@ -27,18 +27,26 @@ export default function App(props) {
 
   return (
     <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
+
+      <nav className="navbar navbar-expand-lg navbar-light bg-light sticky-top">
+        <a className="navbar-brand" href="#"></a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item active">
               <Link to="/">Home</Link>
             </li>
-            <li>
+            <li className="nav-item">
               <Link to="/new">New Post</Link>
             </li>
           </ul>
-        </nav>
+        </div>
+      </nav>
 
+      <div>
         <Switch>
           <Route path="/new">
             <New />
@@ -129,4 +137,7 @@ function Edit() {
       </div>
     </Fragment>
   )
+}
+
+function NavBar() {
 }
