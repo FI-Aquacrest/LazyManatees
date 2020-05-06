845bab32db6e119a185e3ca40e783704e913f58d
FI-Aquacrest <57670730+FI-Aquacrest@users.noreply.github.com>
2020-05-06 14:45:59 +0300

    BUG FIX: remove sneaky "this"

--- app/blog-master/src/main/front/src/App.js
+++ app/blog-master/src/main/front/src/App.js
@@ -97,7 +97,7 @@ export default function App() {
 }
 
 function Login() {
-  return <LoginComponent userLoggedIn={this.userLoggedIn()} />;
+  return <LoginComponent userLoggedIn={userLoggedIn()} />;
 }
 
 function userLoggedIn() {
